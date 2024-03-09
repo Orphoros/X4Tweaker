1c452520b3ca2dba5f333f615a8b85247b556095
Orphoros <orp@orphoros.dev>
2024-03-09 18:25:07 +0100

    feat: add constants

---
+++ src/x4tweaker/lib/constants.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Dlc(Enum):
+    SPLIT       = ('ego_dlc_split', 'Split Vendetta')
+    TERRAN      = ('ego_dlc_terran', 'Cradle of Humanity')
+    PIRATE      = ('ego_dlc_pirate', 'Tides of Avarice')
+    BORON       = ('ego_dlc_boron', 'Kingdom End')
+    TIMELINES   = ('ego_dlc_timelines', 'Timelines')
